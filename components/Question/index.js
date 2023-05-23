1e289ae1f42e4e3e2cefe2dd527738b3db343a7a
IIJTypmaH <prinkos1@gmail.com>
2023-05-24 00:56:37 +0300

    Photo logic in tests

--- components/Question/index.js
+++ components/Question/index.js
@@ -2,74 +2,96 @@ import { useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import Answers from "./Answers";
 import NavigationButton from "./NavigationButton";
+import React from "react";
 
 export default function Question({ questions, hideExercise, finishTest }) {
-    const initialState = {
-        currentQuestion: 0,
-        answers: [],
-        numberOfQuestions: questions.length,
-        correctAnswers: [],
-    };
-    const [state, setState] = useState(initialState);
-    const { currentQuestion, answers, numberOfQuestions } = state;
-    const question = questions[currentQuestion];
+  const initialState = {
+    currentQuestion: 0,
+    answers: [],
+    numberOfQuestions: questions.length,
+    correctAnswers: [],
+  };
+  const [state, setState] = useState(initialState);
+  const { currentQuestion, answers, numberOfQuestions } = state;
+  const question = questions[currentQuestion];
 
+  const submitAnswer = () => {
+    let totalScore = 0;
+    for (let i = 0; i < questions.length; i++) {
+      if (answers[i] === questions[i].correctAnswer) totalScore++;
+    }
+    finishTest(totalScore);
+  };
+  const answerQuestion = (answer) => {
+    answers[currentQuestion] = answer;
+    setState({
+      ...state,
+      answers,
+    });
+  };
 
-    const submitAnswer = () => {
-        let totalScore = 0;
-        for (let i = 0; i < questions.length; i++) {
-            if (answers[i] === questions[i].correctAnswer) totalScore++;
+  const moveQuestion = (direction) => {
+    switch (direction) {
+      case "next": {
+        if (currentQuestion === numberOfQuestions - 1) {
+          submitAnswer();
+          return;
         }
-        finishTest(totalScore);
-    };
-    const answerQuestion = (answer) => {
-        answers[currentQuestion] = answer;
         setState({
-            ...state,
-            answers,
+          ...state,
+          currentQuestion: currentQuestion + 1,
         });
-    };
+        break;
+      }
+      case "prev": {
+        setState({
+          ...state,
+          currentQuestion: currentQuestion - 1,
+        });
+      }
+    }
+  };
 
-    const moveQuestion = (direction) => {
-        switch (direction) {
-            case "next": {
-                if (currentQuestion === numberOfQuestions - 1) {
-                    submitAnswer();
-                    return;
-                }
-                setState({
-                    ...state,
-                    currentQuestion: currentQuestion + 1,
-                });
-                break;
-            }
-            case "prev": {
-                setState({
-                    ...state,
-                    currentQuestion: currentQuestion - 1,
-                });
-            }
-        }
-    };
+  const imgsrc = [
+    {
+      img: "imgs/anger.jpg",
+    },
+    {
+      img: "imgs/contempt.jpg",
+    },
+    {
+      img: "imgs/disgust.jpg",
+    },
+    {
+      img: "imgs/enjoyment.jpg",
+    },
+    {
+      img: "imgs/fear.jpg",
+    },
+  ];
+  return (
+    <div>
+      <button
+        className="flex items-center gap-1 bg-gray-400 p-2 rounded-sm shadow-md text-white"
+        onClick={hideExercise}
+      >
+        <span>
+          <FaArrowLeft />
+        </span>
+        <span>Вернуться</span>
+      </button>
 
-    return (
-        <div>
-            <button className="flex items-center gap-1 bg-gray-400 p-2 rounded-sm shadow-md text-white" onClick={hideExercise}>
-                <span>
-                    <FaArrowLeft />
-                </span>
-                <span>Вернуться</span>
-            </button>
-            <h1 className="text-2xl mt-2 capitalize">{`${
-                state.currentQuestion + 1
-            }. ${question.question}`}</h1>
-            <Answers
-                answers={question.answers}
-                answerQuestion={answerQuestion}
-                state={state}
-            />
+      <h1 className="text-2xl mt-2 capitalize">{`${
+        state.currentQuestion + 1
+      }. ${question.question}`}</h1>
+      <img src={imgsrc[currentQuestion].img}></img>
+      <Answers
+        answers={question.answers}
+        answerQuestion={answerQuestion}
+        state={state}
+      />
 
-            <NavigationButton state={state} moveQuestion={moveQuestion} />
-        </div>
-    );
+      <NavigationButton state={state} moveQuestion={moveQuestion} />
+    </div>
+  );
 }
